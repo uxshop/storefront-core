4ae5fe691a8533a9df65b3d64146899a2b5c2900
diovaniBelleboni <diovani@dooca.com.br>
2022-10-24 14:17:29 -0300

    fix: ajuste em parametro de service

--- src/modules/newsletter/NewsletterService.ts
+++ src/modules/newsletter/NewsletterService.ts
@@ -5,7 +5,7 @@ import { Newsletter, NewsletterFields, NewsletterInput } from './NewsletterTypes
 const Repository = shop_ctx.mock?.newsletter ? NewsletterRepositoryJson : NewsletterRepositoryGql
 
 export class NewsletterService {
-  static async subscribe(userData: NewsletterInput, fields?: NewsletterFields[]): Promise<Newsletter> {
+  static async subscribe(userData: NewsletterInput): Promise<Newsletter> {
     const newsletterResult = await Repository.subscribe(userData)
     return newsletterResult
   }
