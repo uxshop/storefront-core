60ceaff2e2c12a76fe0407fdfe9866defebb740b
diovaniBelleboni <diovani@dooca.com.br>
2022-10-24 15:07:23 -0300

    fix: ajuste teste

--- src/modules/newsletter/NewsletterRepositoryJson.ts
+++ src/modules/newsletter/NewsletterRepositoryJson.ts
@@ -2,11 +2,13 @@ import { Newsletter, NewsletterInput } from './NewsletterTypes'
 
 export class NewsletterRepositoryJson {
   static async subscribe(input: NewsletterInput): Promise<Newsletter> {
-    const mock = shop_ctx.mock?.newsletter
+    const mock = { ...shop_ctx.mock?.newsletter }
 
     if (input.email == mock.email) {
       throw new Error('email already subscribed')
     }
+
+    mock.email = input.email
     return mock || {}
   }
 }
--- src/modules/newsletter/__test__/newsletter.test.ts
+++ src/modules/newsletter/__test__/newsletter.test.ts
@@ -3,14 +3,14 @@ import 'isomorphic-fetch'
 
 describe('Newsletter Module', () => {
   it('Should subscribe to newsletter successfully', async () => {
-    const USER_DATA = { name: 'Test', email: 'test@gmail.com' }
+    const USER_DATA = { name: 'Test', email: 'test2@gmail.com' }
     const newsletterResult = await NewsletterService.subscribe(USER_DATA)
     expect(newsletterResult.name).toEqual(USER_DATA.name)
     expect(newsletterResult.email).toEqual(USER_DATA.email)
   })
 
   it('Should try to subscribe with email already subscribed and it should throw error', async () => {
-    const USER_DATA = { name: 'Test', email: 'test2@gmail.com' }
+    const USER_DATA = { name: 'Test', email: 'test@gmail.com' }
     expect(async () => await NewsletterService.subscribe(USER_DATA)).rejects.toThrow()
   })
 })
