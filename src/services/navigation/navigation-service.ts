c26a1f56144c965cd85065543e95dac33fc985ff
vitorbruski <vitor@dooca.com.br>
2022-10-24 18:38:22 -0300

    fix: literal quotes

--- src/services/navigation/navigation-service.ts
+++ src/services/navigation/navigation-service.ts
@@ -7,29 +7,29 @@ import { BlogCategory } from 'src/modules/blog/category/BlogCategoryTypes'
 import { Page } from 'src/types/PageTypes'
 
 export function getCategoryLink(category: Category): string {
-  return category + '/c'
+  return `${category}/c`
 }
 
 export function getProductLink(product: Product): string {
-  return product + '/p'
+  return `${product}/p`
 }
 
 export function getBrandLink(brand: Brand): string {
-  return brand + '/b'
+  return `${brand}/b`
 }
 
 export function getInstitutionalLink(institutional: Page): string {
-  return institutional + '/i'
+  return `${institutional}/i`
 }
 
 export function getLandgingPageLink(landingPage: LandingPage<LandingPageResponse>): string {
-  return landingPage + '/lp'
+  return `${landingPage}/lp`
 }
 
 export function getBlogPostLink(blogPost: Post): string {
-  return 'blog/' + blogPost
+  return `blog/${blogPost}`
 }
 
 export function getBlogCategoryLink(blogCategory: BlogCategory): string {
-  return 'blog/' + blogCategory
+  return `blog/${blogCategory}`
 }
