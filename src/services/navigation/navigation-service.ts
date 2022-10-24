32ec14be55d49b683a9e2ea26f3257c98b218c07
vitorbruski <vitor@dooca.com.br>
2022-10-24 16:47:01 -0300

    fix: static returns

--- src/services/navigation/navigation-service.ts
+++ src/services/navigation/navigation-service.ts
@@ -7,33 +7,29 @@ import { BlogCategory } from 'src/modules/blog/category/BlogCategoryTypes'
 import { Page } from 'src/types/PageTypes'
 
 export function getCategoryLink(category: Category): string {
-  return '/category/c'
+  return category + '/c'
 }
 
 export function getProductLink(product: Product): string {
-  return '/product/p'
+  return product + '/p'
 }
 
 export function getBrandLink(brand: Brand): string {
-  return '/brand/b'
+  return brand + '/b'
 }
 
 export function getInstitutionalLink(institutional: Page): string {
-  return '/institutional/i'
+  return institutional + '/i'
 }
 
-export function getLandgingPageLink(landing: LandingPage<LandingPageResponse>): string {
-  return '/landing-page/l'
+export function getLandgingPageLink(landingPage: LandingPage<LandingPageResponse>): string {
+  return landingPage + '/lp'
 }
 
 export function getBlogPostLink(blogPost: Post): string {
-  return '/blog-post/p'
+  return 'blog/' + blogPost
 }
 
 export function getBlogCategoryLink(blogCategory: BlogCategory): string {
-  return '/blog-category/c'
-}
-
-export function getLinkByType(item, type: string): string {
-  return '/slug/b'
+  return 'blog/' + blogCategory
 }
