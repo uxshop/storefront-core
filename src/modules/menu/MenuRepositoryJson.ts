49334686cdb69d9054ef2692c607ff5905acced5
diovaniBelleboni <diovani@dooca.com.br>
2022-11-04 14:45:05 -0300

    refactor: ajustes em interfaces e query de menu

--- src/modules/menu/MenuRepositoryJson.ts
+++ src/modules/menu/MenuRepositoryJson.ts
@@ -1,14 +1,14 @@
 import { MenuFields } from './MenuTypes'
 
 export class MenuRepositoryJson {
-  static async getList(fields?: Array<MenuFields>) {
+  static async getList(fields?: MenuFields[]) {
     const result = shop_ctx.mock?.menu
     return result || {}
   }
 
-  static async getById(id: Number, fields?: Array<MenuFields>) {
+  static async getById(id: number, fields?: MenuFields[]) {
     const menus = shop_ctx.mock?.menu
-    const result = menus && menus.filter(menu => menu.id == id)[0]
+    const result = menus && menus.find(menu => menu.id == id)[0]
     return result || {}
   }
 }
--- src/modules/menu/__test__/menu.test.ts
+++ src/modules/menu/__test__/menu.test.ts
@@ -2,13 +2,13 @@ import { Menu, MenuFields } from '../MenuTypes'
 import { MenuService } from '../MenuService'
 import 'isomorphic-fetch'
 
-const ID_FILTER = '47028'
-const SELECTED_FIELDS: Array<MenuFields> = ['id', 'name']
+const ID_FILTER = '47070'
+const SELECTED_FIELDS: MenuFields[] = ['id', 'name']
 
 describe('Menu Module', () => {
   it('Should get menu by id with all fields successfully', async () => {
     const menuResult: Menu = await MenuService.getById(ID_FILTER)
-    expect(menuResult.id).toEqual(ID_FILTER)
+    expect(menuResult.id).toEqual(Number(ID_FILTER))
   })
 
   it('Should get menu by id with selected fields successfully', async () => {
@@ -21,15 +21,15 @@ describe('Menu Module', () => {
   it('Should Get menu list with all fields successfully', async () => {
     const FIRST_ITEM_INDEX = 0
     const menuListResult: Array<Menu> = await MenuService.getList()
-    expect(menuListResult.length).toEqual(2)
-    expect(menuListResult[FIRST_ITEM_INDEX].id).toEqual(ID_FILTER)
+    expect(menuListResult.length).toEqual(1)
+    expect(menuListResult[FIRST_ITEM_INDEX].id).toEqual(Number(ID_FILTER))
   })
 
   it('Should get menu list with all fields successfully', async () => {
     const FIRST_ITEM_INDEX = 0
     const menuListResult: Array<Menu> = await MenuService.getList()
-    expect(menuListResult.length).toEqual(2)
-    expect(menuListResult[FIRST_ITEM_INDEX].id).toEqual(ID_FILTER)
+    expect(menuListResult.length).toEqual(1)
+    expect(menuListResult[FIRST_ITEM_INDEX].id).toEqual(Number(ID_FILTER))
   })
 
   it('Should try to get menu with inexistant id and it should throw error', async () => {
