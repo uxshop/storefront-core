32a78dcfdddfe86f47b35bd24c98e2df40ae955a
diovaniBelleboni <diovani@dooca.com.br>
2022-11-10 15:36:21 -0300

    feat: implementada tratativa de erro e emissao de evento em service de freight

--- src/modules/freight/FreightRepositoryGql.ts
+++ src/modules/freight/FreightRepositoryGql.ts
@@ -6,12 +6,8 @@ export class FreightRepositoryGql {
   static async getList({ fields, filter }: OptionsGetFreight): Promise<Freight[]> {
     const freightQuery = new FreightQueries(fields)
     const getListQuery: string = freightQuery.getListFullQuery()
-    try {
-      const { freights }: FreightResponse = await client.query(getListQuery, filter && { shipping: { ...filter } })
+    const { freights }: FreightResponse = await client.query(getListQuery, filter && { shipping: { ...filter } })
 
-      return freights
-    } catch (error) {
-      throw new Error(error)
-    }
+    return freights
   }
 }
