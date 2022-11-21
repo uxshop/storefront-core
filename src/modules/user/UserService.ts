993614faacdef9e17d08e30b10e664d47620a58a
diovaniBelleboni <diovani@dooca.com.br>
2022-11-21 11:58:22 -0300

    fix: adicionado broadcast service em modulo de user

--- src/modules/user/UserService.ts
+++ src/modules/user/UserService.ts
@@ -1,17 +1,25 @@
 import { UserRepositoryGql } from './UserRepositoryGql'
 import { UserRepositoryJson } from './UserRepositoryJson'
-import { LoginCredentials, OptionsGetUser, User, UserFields } from './UserTypes'
+import { LoginCredentials, User, UserFields } from './UserTypes'
 
 const Repository = shop_ctx.mock?.user ? UserRepositoryJson : UserRepositoryGql
 
 export class UserService {
-  static async auth(credentials: LoginCredentials, fields?: Array<UserFields>): Promise<User> {
-    const result: User = await Repository.auth({ fields: fields || null, credentials: credentials })
-    return result
+  static async auth(credentials: LoginCredentials, fields?: UserFields[]): Promise<User> {
+    try {
+      const result: User = await Repository.auth({ fields: fields || null, credentials: credentials })
+      return result
+    } catch (error) {
+      throw new Error(error?.message)
+    }
   }
 
-  static async get(token: string, fields?: Array<UserFields>): Promise<User> {
-    const result: User = await Repository.get({ fields: fields || null, token: token })
-    return result
+  static async get(token: string, fields?: UserFields[]): Promise<User> {
+    try {
+      const result: User = await Repository.get({ fields: fields || null, token: token })
+      return result
+    } catch (error) {
+      throw new Error(error?.message)
+    }
   }
 }
