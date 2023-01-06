261b410808cd92a70304abe6275c9f6cf45e770e
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2023-01-06 08:55:23 -0300

    fix: coloca eventos em um objeto

--- src/modules/slug/SlugService.ts
+++ src/modules/slug/SlugService.ts
@@ -2,14 +2,34 @@ import { SlugRepositoryGql } from './SlugRepositoryGql'
 import { SlugRepositoryJson } from './SlugRepositoryJson'
 import { Slug, SlugFilter } from './SlugType'
 import { BroadcastService } from '../../services/broadcast/broadcast-service'
-import { BroadcastServiceEmitType } from '../../types/BroadcastEventEmitType'
 const Repository = () => (shop_ctx.mock?.slug ? SlugRepositoryJson : SlugRepositoryGql)
+
+const broadcastServiceEmitType = {
+  settings: 'Settings',
+  showcase: 'Showcase',
+  brand: 'Brand',
+  'landing-page': 'Landingpage',
+  apps: 'Apps',
+  blog: 'Blog',
+  cart: 'Cart',
+  category: 'Category',
+  freight: 'Freight',
+  menu: 'Menu',
+  newsletter: 'Newsletter',
+  pages: 'Pages',
+  product: 'Product',
+  scripts: 'Scripts',
+  sections: 'Sections',
+  shop: 'Shop',
+  sidebar: 'Sidebar',
+  user: 'User'
+}
 
 export class SlugService {
   static async getOne(filter?: SlugFilter): Promise<Slug> {
     try {
       const result = await Repository().getOne(filter)
-      const eventType = BroadcastServiceEmitType[result.source]
+      const eventType = broadcastServiceEmitType[result.source]
       BroadcastService.emit(eventType, result)
       return result
     } catch (error) {
