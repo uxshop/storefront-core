b25974bfa9f16eef10dc522a1ca83b0f6d19391a
simaomeyrer <simaomeyrer@gmail.com>
2023-09-29 10:08:52 -0300

    fix: remove default apis

--- src/services/ShopService.ts
+++ src/services/ShopService.ts
@@ -2,27 +2,15 @@ import { ShopService } from '../modules/shop/ShopService'
 import { Shop } from '../modules/shop/ShopTypes'
 import { ShopConfig, ShopDataConfig } from '../types/ShopTypes'
 
-const URL_APIS = {
-  homolog: {
-    api_url: 'https://api.storefront.dchomolog.dooca.store/',
-    api_editor: 'https://api.editor.dchomolog.dooca.store/'
-  },
-  production: {
-    api_url: 'https://api.storefront.dooca.store/',
-    api_editor: 'https://api.editor.dooca.store/'
-  }
-}
-
 export class ShopConfigService {
   shop: Shop
 
   constructor(private config: ShopConfig) {}
 
   private getApisUrl() {
-    const defaultApis = import.meta.env.MODE !== 'production' ? URL_APIS.homolog : URL_APIS.production
     const apis = {
-      api_editor: this.config.apiEditor || defaultApis.api_editor,
-      api_url: this.config.apiUrl || defaultApis.api_url
+      api_editor: this.config.apiEditor,
+      api_url: this.config.apiUrl
     }
     return apis
   }
