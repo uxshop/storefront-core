f56a5d879c496818ac02abed366d6f058020d64d
simaomeyrer <simaomeyrer@gmail.com>
2023-10-04 16:56:04 -0300

    feat: ajuste do type

--- src/modules/buy-together/BuyTogetherRepositoryGql.ts
+++ src/modules/buy-together/BuyTogetherRepositoryGql.ts
@@ -4,8 +4,11 @@ import { BuyTogetherQueries } from './BuyTogetherQueries'
 
 export class BuyTogetherRepositoryGql {
   static async getByProductId(productId: number, fields?: BuyTogetherFields[]): Promise<BuyTogether> {
-    const buyTogether = new BuyTogetherQueries(fields)
-    const buyTogetherGetOneQuery = buyTogether.getOneFullQuery()
-    return await getClient().query(buyTogetherGetOneQuery, { filter: { productId } })
+    const buyTogetherQuery = new BuyTogetherQueries(fields)
+    const buyTogetherGetOneQuery = buyTogetherQuery.getOneFullQuery()
+    const { buyTogether } = (await getClient().query(buyTogetherGetOneQuery, { filter: { productId } })) as {
+      buyTogether: BuyTogether
+    }
+    return buyTogether
   }
 }
