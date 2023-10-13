3a41cfdc2b7878b28092e013519736ea5bd530f1
simaomeyrerdooca <simao.meyrer@bagy.com.br>
2023-10-13 16:06:58 -0300

    fix(buy-together): função com validação de data válida

--- src/modules/buy-together/BuyTogetherService.ts
+++ src/modules/buy-together/BuyTogetherService.ts
@@ -5,4 +5,22 @@ export class BuyTogetherService {
   static async getByProductId(productId: number, fields?: BuyTogetherFields[]): Promise<BuyTogether> {
     return await BuyTogetherRepositoryGql.getByProductId(productId, fields)
   }
+
+  static async getByProductIdWithValidPromotionDate(
+    productId: number,
+    fields?: BuyTogetherFields[]
+  ): Promise<BuyTogether> {
+    const buyTogetherData = await BuyTogetherRepositoryGql.getByProductId(productId, fields)
+    console.log('🚀 ~ file: BuyTogetherService.ts:14 ~ BuyTogetherService ~ buyTogetherData:', buyTogetherData)
+    if (buyTogetherData && this.checkPromotionDateValid(buyTogetherData)) return buyTogetherData
+    return null
+  }
+
+  private static checkPromotionDateValid({ dateFrom, dateTo }: BuyTogether) {
+    const nowUnixTime = Number(new Date())
+    const dateFromUnixTime = Number(new Date(dateFrom + 'T00:00:00'))
+    if (!dateTo) return nowUnixTime >= dateFromUnixTime
+    const dateToUnixTime = Number(new Date(dateTo + 'T00:00:00'))
+    return nowUnixTime >= dateFromUnixTime && nowUnixTime <= dateToUnixTime
+  }
 }
